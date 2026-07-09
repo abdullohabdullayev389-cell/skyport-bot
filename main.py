8ca83f4dd368c40dbb8135c9b4238d351f208721
abdullohabdullayev389-cell <abdullohabdullayev389@gmail.com>
2026-07-09 07:18:11 +0500

    Update main.py

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ logging.basicConfig(level=logging.INFO)
 
 # ─── 1. BAZA VA MODELLAR SOZLAMASI ───
 DATABASE_URL = os.getenv("DATABASE_URL")
+if DATABASE_URL and DATABASE_URL.startswith("postgresql://"):
+    DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+asyncpg://", 1)
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 engine = create_async_engine(DATABASE_URL, echo=False)
